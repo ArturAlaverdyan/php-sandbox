b7e741294d6138394bd425975dddb7c6b9e88c50
Artur <alaverdyan.oa@gmail.com>
2024-11-17 22:47:07 +0300

    feat(sort): implement quicksort alg

---
+++ Testing/Coper.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Testing;
+
+class Coper
+{
+public static function run(){
+    return 5;
+}
+}
---
+++ Testing/Second.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Testing;
+
+class Second
+{
+public static function doSomething(){
+    return 111;
+}
+
+}
